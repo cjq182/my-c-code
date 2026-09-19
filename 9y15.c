aac865f207903957d9768c6f75523f126b7ad222
chenggao <c13180036168@163.com>
2026-09-19 23:37:11 +0800

    Add C study programs

---
+++ 9y15.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	 printf("100\n");
+	 return 0;
+}
---
+++ 9y16.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	const int amount = 100;
+	int price = 0;
+	
+	printf("请输入金额（元）：");
+	scanf("%d", &price);
+	int change =amount - price;
+	printf("找零%d元。\n", change);
+	return 0;
+}
+
